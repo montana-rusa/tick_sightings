5aa24d6c866394a535a441ba5435ff165a1d8db2
Emma <emlcliffe@gmail.com>
2025-11-21 19:15:52 +0000

    adding graph containers to ui

--- project/app.py
+++ project/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+
+    labels = ['January', 'February', 'March', 'April', 'May', 'June']
+    data = [0, 10, 15, 8, 22, 18, 25]
+
+    return render_template('home.html', labels=labels, data=data)
 if __name__ == '__main__':
     app.run()
 
