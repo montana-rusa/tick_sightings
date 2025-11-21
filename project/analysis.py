fff57a3966f7365fa990f7a7845f245e7cd725e9
Emma <emlcliffe@gmail.com>
2025-11-21 20:46:26 +0000

    ui changes

--- project/analysis.py
+++ project/analysis.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("data/data.csv")
+df = pd.read_csv('data.csv')
+
+# the dataset has no duplicates or incomplete row
+
+#plan: have two graphs, the bar showing 
+
+df['date'] = df['date'].str[:10]
+df['date'] = pd.to_datetime(df['date']) 
 print(df)
