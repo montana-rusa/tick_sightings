4b96708619d426df15c1ceea88f55b36d1bc8e5f
Emma <emlcliffe@gmail.com>
2025-11-22 18:00:59 +0000

    both graphs now working

--- project/analysis.py
+++ project/analysis.py
@@ -13,24 +13,41 @@ df['date'] = pd.to_datetime(df['date'])
 #print(df)
 
 
-def filter_by_time(start = False, end = False, df=df):
+def filter_by_time(start = None, end = None, df=df):
 
-    if start != False:
+    if start != None:
         regional = df[(df['date'] > start) & (df['date'] < end)]
     else:
-        regional = df
+        regional = df.copy()
 
-    
-   
-    #print(cutoff.date())
     regional = regional.groupby(['location']).count()
     regional = regional.sort_values(by="id", ascending=False)
-    #print(regional)
-
-
-
+    
     x = regional.index.to_list()
     y = regional['id'].to_list()
 
     return (x, y)
+
+def filter_by_location(location=None, df=df):
+
+    if location == None:
+        month_df = df.copy()
+    else:
+        month_df = df[df['location'] == location]
+
+
+    month_df['year'] = month_df['date'].dt.year
+    month_df['month'] = month_df['date'].dt.month
+    month_df = month_df.sort_values(by=['year','month'], ascending=True).reset_index(drop=True)
+    month_df = month_df.groupby(['year','month']).count()
+
+    y = np.zeros(156) #change to reflect number of months in the time period shown by the data
+
+    for date, count in zip(month_df.index, month_df['id']):
+        y[((date[0] - 2012)*12) + date[1]-1] = count
+    x = list(range(len(y)))
+    y = list(y)
+
+    return (x, y)
+
 
--- project/app.py
+++ project/app.py
@@ -4,16 +4,23 @@ import pandas as pd
 
 app = Flask(__name__)
 
+start, end, location = None
+
+
 @app.route("/")
 def home():
 
     barColors = ["red", "green","blue","orange","brown"];
 
     xValues, yValues = analysis.filter_by_time()
-    return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors)
+    xValues2, yValues2 = analysis.filter_by_location()
 
-@app.route("/submit", methods =["GET", "POST"])
-def submit():
+
+
+    return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors, xValues2 = xValues2, yValues2 = yValues2)
+
+@app.route("/submit1", methods =["GET", "POST"])
+def submit1():
 
     if request.method == "GET":
         return redirect("/")
@@ -24,7 +31,9 @@ def submit():
     end = pd.to_datetime(request.form['end'])
 
     xValues, yValues = analysis.filter_by_time(start, end)
-    return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors)
+    xValues2, yValues2 = analysis.filter_by_location()
+
+    return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors, xValues2 = xValues2, yValues2 = yValues2)
 
 if __name__ == '__main__':
     app.run(debug=True)
