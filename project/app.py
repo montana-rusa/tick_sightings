820eb28e9d1e3918f8383291c503b4fc277cea77
Emma <emlcliffe@gmail.com>
2025-11-22 19:22:05 +0000

    cleaning up

--- project/app.py
+++ project/app.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
-start, end, location = None
-
+start = None
+end = None
+location = None
+barColors = ["red", "green","blue","orange","brown"];
 
 @app.route("/")
 def home():
@@ -15,8 +17,6 @@ def home():
     xValues, yValues = analysis.filter_by_time()
     xValues2, yValues2 = analysis.filter_by_location()
 
-
-
     return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors, xValues2 = xValues2, yValues2 = yValues2)
 
 @app.route("/submit1", methods =["GET", "POST"])
@@ -25,13 +25,28 @@ def submit1():
     if request.method == "GET":
         return redirect("/")
 
-    barColors = ["red", "green","blue","orange","brown"];
-
     start = pd.to_datetime(request.form['start'])
     end = pd.to_datetime(request.form['end'])
 
     xValues, yValues = analysis.filter_by_time(start, end)
-    xValues2, yValues2 = analysis.filter_by_location()
+    xValues2, yValues2 = analysis.filter_by_location(location)
+
+    return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors, xValues2 = xValues2, yValues2 = yValues2)
+
+
+@app.route("/submit2", methods =["GET", "POST"])
+def submit2():
+
+    if request.method == "GET":
+        return redirect("/")
+
+    location = str(request.form['location'])
+
+    if location == "All":
+        location = None
+
+    xValues, yValues = analysis.filter_by_time(start, end)
+    xValues2, yValues2 = analysis.filter_by_location(location)
 
     return render_template('home.html', xValues=xValues, yValues=yValues, barColors=barColors, xValues2 = xValues2, yValues2 = yValues2)
 
